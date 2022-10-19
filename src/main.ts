0a102e8ab074311a2eb9a449f1b8fca93e04bf59
TimPrevotPro <timothe.prevot@efrei.net>
2022-10-19 17:25:54 +0200

    fix: Added some conditions to avoid issues
    mainly to avoid issues with out of bounds mountains/treasures/adventurers

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,8 @@ import Adventurer from "./adventurer";
 const fileName: string = "entry_files/testfile1.txt";
 // On lit le fichier et stocke son contenu dans une String
 let fileContent = fs.readFileSync(fileName, 'utf-8');
-console.log("file : ", fileContent);
 // On splitte la String pour séparer les différentes lignes
 const splitFile: string[] = fileContent.split("\r\n");
-
 
 // Variables pour stocker les dimensions de la carte
 let verticalLength = 0;
@@ -37,23 +35,34 @@ for (let i = 0; i < splitFile.length; i++) {
             break;
         // Pour chaque montagne, on crée une instance de Montagne que l'on stocke dans le tableau correspondant
         case 'M':
-            mountains.push(new Mountain(parseInt(splitFile[i][2]), parseInt(splitFile[i][4])));
+            if (0 <= parseInt(splitFile[i][2]) && parseInt(splitFile[i][2]) <= horizontalLength && 0 <= parseInt(splitFile[i][4]) && parseInt(splitFile[i][4]) <= verticalLength) {
+                mountains.push(new Mountain(parseInt(splitFile[i][2]), parseInt(splitFile[i][4])));
+            } else {
+                console.log("La montagne en (", splitFile[i][2], ",", splitFile[i][4], ") est hors limites !");
+            }
             break;
 
         // Pour chaque trésor, on crée une instance de TreasureSpot que l'on stocke dans le tableau correspondant
         case 'T':
-            treasureSpots.push(new TreasureSpot(parseInt(splitFile[i][2]), parseInt(splitFile[i][4]), parseInt(splitFile[i][6])));
+            if (0 <= parseInt(splitFile[i][2]) && parseInt(splitFile[i][2]) <= horizontalLength && 0 <= parseInt(splitFile[i][4]) && parseInt(splitFile[i][4]) <= verticalLength) {
+                treasureSpots.push(new TreasureSpot(parseInt(splitFile[i][2]), parseInt(splitFile[i][4]), parseInt(splitFile[i][6])));
+            } else {
+                console.log("Le trésor en (", splitFile[i][2], ",", splitFile[i][4], ") est hors limites !");
+            }
             break;
 
         // Pour chaque aventurier, on crée une instance de Adventurer que l'on stocke dans le tableau correspondant
         case 'A':
             const adventurerParams: string[] = splitFile[i].split("-");
-            adventurers.push(new Adventurer(adventurerParams[1], parseInt(adventurerParams[2]), parseInt(adventurerParams[3]),
-                adventurerParams[4], adventurerParams[5]));
-
-            // On stocke aussi les maxMoves
-            if (adventurerParams[5].length > maxMoves)
-                maxMoves = adventurerParams[5].length;
+            if (0 <= parseInt(adventurerParams[2]) && parseInt(adventurerParams[2]) <= horizontalLength && 0 <= parseInt(adventurerParams[3]) && parseInt(adventurerParams[3]) <= verticalLength) {
+                adventurers.push(new Adventurer(adventurerParams[1], parseInt(adventurerParams[2]), parseInt(adventurerParams[3]),
+                    adventurerParams[4], adventurerParams[5]));
+                // On stocke aussi les maxMoves
+                if (adventurerParams[5].length > maxMoves)
+                    maxMoves = adventurerParams[5].length;
+            } else {
+                console.log("L'aventurier ", adventurerParams[1], " en (", adventurerParams[2], ",", adventurerParams[3], ") est hors limites !");
+            }
             break;
         default:
             break;
@@ -73,7 +82,7 @@ while (maxMoves > 0) {
                     // On vérifie si une montagne bloque le déplacement de l'aventurier
                     mountains.forEach((mountain) => {
                         if (mountain.horizontalPos === adventurer.horizontalPos && mountain.verticalPos === adventurer.verticalPos - 1) {
-                            console.log(adventurer.name, " est bloqué(e) par une montagne !");
+                            console.log(adventurer.name, "est bloqué(e) par une montagne !");
                             noMountains = false;
                         }
                     });
@@ -81,7 +90,7 @@ while (maxMoves > 0) {
                     // On vérifie si un autre aventurier bloque le déplacement de l'aventurier
                     adventurers.forEach((competitor) => {
                         if (competitor.horizontalPos === adventurer.horizontalPos && competitor.verticalPos === adventurer.verticalPos - 1) {
-                            console.log(competitor.name, " bloque ", adventurer.name, " !");
+                            console.log(competitor.name, "bloque ", adventurer.name, " !");
                             noCompetitors = false;
                         }
                     });
@@ -94,7 +103,7 @@ while (maxMoves > 0) {
                     // On vérifie si une montagne bloque le déplacement de l'aventurier
                     mountains.forEach((mountain) => {
                         if (mountain.horizontalPos === adventurer.horizontalPos && mountain.verticalPos === adventurer.verticalPos + 1) {
-                            console.log(adventurer.name, " est bloqué(e) par une montagne !");
+                            console.log(adventurer.name, "est bloqué(e) par une montagne !");
                             noMountains = false;
                         }
                     });
@@ -102,7 +111,7 @@ while (maxMoves > 0) {
                     // On vérifie si un autre aventurier bloque le déplacement de l'aventurier
                     adventurers.forEach((competitor) => {
                         if (competitor.horizontalPos === adventurer.horizontalPos && competitor.verticalPos === adventurer.verticalPos + 1) {
-                            console.log(competitor.name, " bloque ", adventurer.name, " !");
+                            console.log(competitor.name, "bloque ", adventurer.name, " !");
                             noCompetitors = false;
                         }
                     });
@@ -115,7 +124,7 @@ while (maxMoves > 0) {
                     // On vérifie si une montagne bloque le déplacement de l'aventurier
                     mountains.forEach((mountain) => {
                         if (mountain.horizontalPos === adventurer.horizontalPos - 1 && mountain.verticalPos === adventurer.verticalPos) {
-                            console.log(adventurer.name, " est bloqué(e) par une montagne !");
+                            console.log(adventurer.name, "est bloqué(e) par une montagne !");
                             noMountains = false;
                         }
                     });
@@ -123,7 +132,7 @@ while (maxMoves > 0) {
                     // On vérifie si un autre aventurier bloque le déplacement de l'aventurier
                     adventurers.forEach((competitor) => {
                         if (competitor.horizontalPos === adventurer.horizontalPos - 1 && competitor.verticalPos === adventurer.verticalPos) {
-                            console.log(competitor.name, " bloque ", adventurer.name, " !");
+                            console.log(competitor.name, "bloque ", adventurer.name, " !");
                             noCompetitors = false;
                         }
                     });
@@ -136,7 +145,7 @@ while (maxMoves > 0) {
                     // On vérifie si une montagne bloque le déplacement de l'aventurier
                     mountains.forEach((mountain) => {
                         if (mountain.horizontalPos === adventurer.horizontalPos + 1 && mountain.verticalPos === adventurer.verticalPos) {
-                            console.log(adventurer.name, " est bloqué(e) par une montagne !");
+                            console.log(adventurer.name, "est bloqué(e) par une montagne !");
                             noMountains = false;
                         }
                     });
@@ -144,7 +153,7 @@ while (maxMoves > 0) {
                     // On vérifie si un autre aventurier bloque le déplacement de l'aventurier
                     adventurers.forEach((competitor) => {
                         if (competitor.horizontalPos === adventurer.horizontalPos + 1 && competitor.verticalPos === adventurer.verticalPos) {
-                            console.log(competitor.name, " bloque ", adventurer.name, " !");
+                            console.log(competitor.name, "bloque ", adventurer.name, " !");
                             noCompetitors = false;
                         }
                     });
@@ -208,13 +217,15 @@ while (maxMoves > 0) {
             if (treasureSpot.horizontalPos === adventurer.horizontalPos && treasureSpot.verticalPos === adventurer.verticalPos && treasureSpot.nbTreasures > 0) {
                 treasureSpot.nbTreasures--;
                 adventurer.treasures++;
-                console.log(adventurer.name, " a trouvé un trésor en (", treasureSpot.horizontalPos, ", ", treasureSpot.verticalPos, ") !");
+                console.log(adventurer.name, "a trouvé un trésor en (", treasureSpot.horizontalPos, ", ", treasureSpot.verticalPos, ") !");
             }
         })
     })
     maxMoves--;
 }
 
+// On utilise writeFileSync() en premier pour effacer le précédent contenu du fichier de sortie
+// Puis on utilise appendFileSync() pour simplement ajouter une ligne au fichier
 fs.writeFileSync('exitFiles/finaltest1.txt', splitFile[0] + '\n');
 mountains.forEach((mountain) => {
     fs.appendFileSync('exitFiles/finaltest1.txt', 'M-' + mountain.horizontalPos + '-' + mountain.verticalPos + '\n');
